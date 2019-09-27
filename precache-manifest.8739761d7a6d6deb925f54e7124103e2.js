f5954f9b3e179c808fec771214a2ca97edee4755
abazunts home <bazunc@gmail.com>
2019-09-28 03:14:26 +0500

    Updates

---
+++ precache-manifest.8739761d7a6d6deb925f54e7124103e2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "7668e86994fe56fad33f",
+    "url": "/testless/static/css/main.e35abfc9.chunk.css"
+  },
+  {
+    "revision": "7668e86994fe56fad33f",
+    "url": "/testless/static/js/main.7668e869.chunk.js"
+  },
+  {
+    "revision": "6af4908f4cbcef8887fb",
+    "url": "/testless/static/css/1.4bb9673e.chunk.css"
+  },
+  {
+    "revision": "6af4908f4cbcef8887fb",
+    "url": "/testless/static/js/1.6af4908f.chunk.js"
+  },
+  {
+    "revision": "e2eda0b75baf6e66a0a6",
+    "url": "/testless/static/js/runtime~main.e2eda0b7.js"
+  },
+  {
+    "revision": "91ed980154af565deeb380eb66dddc1d",
+    "url": "/testless/static/media/ltbukra-bold-webfont.91ed9801.woff"
+  },
+  {
+    "revision": "e8cd3e353d695217bdece78cea00e460",
+    "url": "/testless/static/media/ltbukra-bold-webfont.e8cd3e35.woff2"
+  },
+  {
+    "revision": "4972b374fc52777714360ce13925d2e4",
+    "url": "/testless/static/media/ltbukra-bold-webfont.4972b374.eot"
+  },
+  {
+    "revision": "a1c38193bcbeec87253eada26c3c6dc8",
+    "url": "/testless/static/media/ltbukra-bold-webfont.a1c38193.ttf"
+  },
+  {
+    "revision": "706c12cd19784c7ccf2bbd4113c287be",
+    "url": "/testless/static/media/ltbukra-bold-webfont.706c12cd.svg"
+  },
+  {
+    "revision": "6495e18842b74e7cba070d9af9074cea",
+    "url": "/testless/static/media/ltbukra-regular-webfont.6495e188.woff2"
+  },
+  {
+    "revision": "62ac976ecd2f557ea5ee085d8d1bb256",
+    "url": "/testless/static/media/ltbukra-regular-webfont.62ac976e.woff"
+  },
+  {
+    "revision": "e050d364fd222620de26f7ad06aa5b64",
+    "url": "/testless/static/media/ltbukra-regular-webfont.e050d364.ttf"
+  },
+  {
+    "revision": "13afdf88572fad03419fc4c382e19438",
+    "url": "/testless/static/media/ltbukra-regular-webfont.13afdf88.eot"
+  },
+  {
+    "revision": "9cb761ca91c3fe6c7dde8af50f1b4fa9",
+    "url": "/testless/static/media/icomoon.9cb761ca.eot"
+  },
+  {
+    "revision": "bb33c6d336301712e2b0ba2ff9b3574b",
+    "url": "/testless/static/media/icomoon.bb33c6d3.ttf"
+  },
+  {
+    "revision": "8420933c828141c14921c368e8667a3b",
+    "url": "/testless/static/media/icomoon.8420933c.woff"
+  },
+  {
+    "revision": "ebed3846a3b77fd4680f54c75dd9aa2d",
+    "url": "/testless/static/media/ltbukra-regular-webfont.ebed3846.svg"
+  },
+  {
+    "revision": "57d096f206aefbe7c3770e2ba81cb23e",
+    "url": "/testless/static/media/icomoon.57d096f2.svg"
+  },
+  {
+    "revision": "81290c84bbe762228faedbde0034d6d7",
+    "url": "/testless/static/media/card-icon-bg.81290c84.svg"
+  },
+  {
+    "revision": "433bea8da239546b460a711eedc968f4",
+    "url": "/testless/static/media/logo.433bea8d.svg"
+  },
+  {
+    "revision": "dd606c863b4d0356ef994a9856a0d011",
+    "url": "/testless/static/media/logo-sm.dd606c86.svg"
+  },
+  {
+    "revision": "d7e60f9d1433a45ed71817f6d23abeca",
+    "url": "/testless/static/media/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.d7e60f9d.woff2"
+  },
+  {
+    "revision": "c29f62e88796ae987f55a139951cd17e",
+    "url": "/testless/index.html"
+  }
+];
